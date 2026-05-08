6d62ee9521630a27a101a2653fcc0ec18682847e
Lucas Galli <luquigalli0207@gmail.com>
2026-05-08 13:25:35 -0300

    add crane and crane hinges

---
+++ Assets/Scripts/CraneMovement.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CraneMovement : MonoBehaviour
+{
+    [SerializeField] private int MaxMovement = 10;
+    private float xPosition = 2f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
