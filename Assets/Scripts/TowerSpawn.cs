413a18c8f41c53a3000a8be029fa9e6d80f0c04f
Lucas Galli <luquigalli0207@gmail.com>
2026-05-11 11:52:11 -0300

    add block prefab and spawner function

--- Assets/Scripts/TowerSpawn.cs
+++ Assets/Scripts/TowerSpawn.cs
@@ -1,16 +1,28 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TowerSpawn : MonoBehaviour
 {
+    [SerializeField] public GameObject BlockPrefab;
+    [SerializeField] public HingeJoint blockJoint;
+    [SerializeField] public Rigidbody crane;
+    private GameObject Clone;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Clone = Instantiate(BlockPrefab, transform);
+        blockJoint = Clone.GetComponent<HingeJoint>();
+        blockJoint.connectedBody = crane;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            Clone.transform.SetParent(null);
+            Destroy(blockJoint);
+        }
     }
 }
